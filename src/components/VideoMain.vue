f170a1a74a2b40cd91fc36972d90b337c4cf224e
pokr <mistrpokr@outlook.com>
2021-04-23 21:19:27 +0800

    Switch to dummy video for preview

--- src/components/VideoMain.vue
+++ src/components/VideoMain.vue
@@ -18,10 +18,12 @@ export default {
         autoplay: true,
         controls: true,
         sources: [{
+          src: "http://vjs.zencdn.net/v/oceans.mp4",
+          type:"video/mp4",
           // src: "http://192.168.0.138:8001/live/py/index.mpd",
           // type: "application/dash+xml",
-          src: "http://192.168.0.138:8001/live/py/index.m3u8",
-          type: "application/x-mpegURL",
+          // src: "http://192.168.0.138:8001/live/py/index.m3u8",
+          // type: "application/x-mpegURL",
         }]
       }
     }
