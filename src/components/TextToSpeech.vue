3d62d5e82074f22c0049da1b17ecfc2a813be075
pokr <mistrpokr@outlook.com>
2021-05-07 14:57:57 +0800

    feat:tts card as component,send post to api

---
+++ src/components/TextToSpeech.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-card class="mb-4">
+    <v-card-title>
+      Voice Controls
+    </v-card-title>
+    <!--      one action component per v-card-actions-->
+    <v-divider></v-divider>
+    <v-card-actions class="mx-2">
+      <v-form>
+        <v-select
+            v-model="ttsPrerecorded"
+            :items="selectPrerecordedOptions"
+            item-text="text"
+            item-value="value"
+        >
+        </v-select>
+        <v-text-field
+            id="textarea"
+            v-model="messageText"
+            placeholder="Enter something..."
+            rows="3"
+            max-rows="6"
+            v-if="!ttsPrerecorded"
+        >
+        </v-text-field>
+        <v-radio-group v-if="ttsPrerecorded" v-model="speechChoice">
+          <v-radio
+              v-for="str in speechOptions"
+              :key="str"
+              :label="str"
+              :value="str"
+          >
+          </v-radio>
+        </v-radio-group>
+      </v-form>
+    </v-card-actions>
+    <v-card-actions class="mx-2">
+      <v-btn color="primary" @click="voiceHandler">
+        SUBMIT
+      </v-btn>
+      <v-btn color="">RESET</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "TextToSpeech",
+  data() {
+    return {
+      messageText: null,
+      ttsPrerecorded: true,
+      speechOptions: ["A lovely evening! ", "How's it going? "],
+      speechChoice: "",
+      selectPrerecordedOptions: [
+        {value: true, text: "Prerecorded Text"},
+        {value: false, text: "Custom Text"},
+      ],
+    }
+  },
+  methods: {
+    voiceHandler: function () {
+      if (this.ttsPrerecorded) {
+        //Use prerecorded voice or upload file
+        axios
+            .post()
+            .then()
+      } else {
+        // Sends text for TTS
+        let newFormData = new FormData()
+        newFormData.append("text", this.messageText)
+        axios({
+          method: "post",
+          url: "http://localhost:8000/voice/tts/",
+          data: newFormData,
+          headers: {"Content-Type": "multipart/form-data"},
+        })
+            .then(function (response) {
+              console.log(response)
+            })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
