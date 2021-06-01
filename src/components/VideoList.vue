e4ccf07765bc0ce8069edaa92c828c2aa266d1d4
pokr <mistrpokr@outlook.com>
2021-06-01 23:12:41 +0800

    feat:bind Download button to actual API

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -71,7 +71,21 @@ export default {
           })
     },
     downloadVideo: function (item) {
-      console.log(item.name);
+      axios({
+        url: `/video/download/${item.name}`,
+        method: 'GET',
+        responseType: 'blob',
+      }).then((response) => {
+        var fileURL = window.URL.createObjectURL(new Blob([response.data]));
+        var fileLink = document.createElement('a');
+
+        fileLink.href = fileURL;
+        fileLink.setAttribute('download', 'file.mp4');
+        document.body.appendChild(fileLink);
+
+        fileLink.click();
+      });
+
     },
     deleteVideo: function (item) {
       console.log(item.name);
