87ac2305cb5b8589f1752d1a27b5f01cd60426bb
pokr <mistrpokr@outlook.com>
2021-06-01 22:01:29 +0800

    feat(wip):add video list layout;update nav bar

---
+++ src/components/VideoList.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card>
+    <v-card-title>
+      视频列表
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-data-table>
+
+      </v-data-table>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "VideoList",
+  data() {
+    return {
+      videoList: null,
+    }
+  },
+  methods: {
+    updateVideoList: function () {
+      let that = this;
+      axios
+          .get("/video/list/")
+          .then(function (response) {
+            that.videoList = response.data;
+          })
+    }
+  },
+  mounted() {
+    this.updateVideoList();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
