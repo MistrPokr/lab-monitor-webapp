d1ad31c0dcbdc71f810cfe597a22066bd1f10f18
pokr <mistrpokr@outlook.com>
2021-06-03 16:45:08 +0800

    feat:add live restart button & warning popup

--- src/components/Stream.vue
+++ src/components/Stream.vue
@@ -9,8 +9,36 @@
       <b>在线</b>
     </v-card-text>
     <v-card-actions>
-      <v-btn color="red" @click="restartStream">重启直播</v-btn>
-      <v-btn color="primary" @click="reloadPlayer">刷新播放器</v-btn>
+      <v-dialog
+          v-model="warningDialog"
+          width="500"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" v-bind="attrs" v-on="on">重启直播</v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="">
+            提示
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-card-text class="mt-4">
+            重启时将短暂中断后台视频录制，可能产生至多数十秒的画面缺失。
+            <br/>
+            是否确认重启？
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                color="red"
+                @click="restartClickWrapper"
+            >
+              确定
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-btn class="mx-2" @click="reloadPlayer">刷新播放器</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -21,9 +49,15 @@ import axios from "axios";
 export default {
   name: "Stream",
   data() {
-    return {};
+    return {
+      warningDialog: false,
+    };
   },
   methods: {
+    restartClickWrapper(){
+      this.warningDialog = false;
+      this.restartStream();
+    },
     restartStream() {
       axios
           .post("/api/live/")
